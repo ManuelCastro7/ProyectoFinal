41d7bea0fb78caa267f9d8850547ac74ef9ffac6
Manuel Castro <manuel.castro.ofc@gmail.com>
2023-01-21 14:10:46 -0300

    En esta ocacion, he agregado unas cards para los cursos realizados, e incorporado una funcion para el boton de redes sociales

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("boton").addEventListener("click", function(){
+    document.getElementById("redesSociales").style = "display: block";
+    document.getElementById("redesSociales").style = "flex-direction: row";
+    document.getElementById("boton").style = "display: none";
+});
