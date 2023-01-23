d53173a11be3cd74106635315b78b160746fee2a
Manuel Castro <manuel.castro.ofc@gmail.com>
2023-01-23 12:14:51 -0300

    Agregado una nueva funcionalidad

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+let repetir = true;
+
+while (repetir) {
+    let edad = parseFloat(prompt("Ingrese su edad."))
+
+    if (edad >= 18){
+        break
+    }
+    else if(edad <17){
+        alert("Debes ser mayor de edad.")
+    }
+}
+
 document.getElementById("boton").addEventListener("click", function(){
     document.getElementById("redesSociales").style = "display: block";
     document.getElementById("redesSociales").style = "flex-direction: row";
